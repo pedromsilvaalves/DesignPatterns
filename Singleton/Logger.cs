b0a1ba21f21e45c2ce07d8a32d711634e35c155f
Pedro Alves <pedromelosilvaalves@outlook.com>
2020-01-21 19:49:41 -0300

    Updating Singleton example to a Logger

---
+++ Singleton/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singleton
+{
+    public sealed class Logger
+    {
+        private static readonly Logger instancia = new Logger();
+        static Logger() {
+            Console.WriteLine("Datetime: {0} - Static - Logger Criado", DateTime.Now);
+        }
+        private Logger() {
+            Console.WriteLine("Datetime: {0} - Private - Logger Criado", DateTime.Now);
+        }
+
+        public static Logger GetLogger
+        {
+            get
+            {
+                return instancia;
+            }
+        }
+
+        public void Logar(string mensagem)
+        {
+            Console.WriteLine("Datetime: {0} - Mensagem: {1}", DateTime.Now, mensagem);
+        }
+    }
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Singleton
 {
@@ -6,14 +7,34 @@ namespace Singleton
     {
         static void Main(string[] args)
         {
-            Singleton jogador1 = Singleton.GetInstancia;
-            jogador1.Mensagem("Jogador 1: A bola esta comigo no meio do campo, passei a bola");
+            string[] eventosPartida =
+            {
+                "Jogador 1: A bola esta comigo no meio do campo, passei a bola",
+                "Jogador 2: Recebeu a bola, fiz o lancamento",
+                "Jogador 3: Recebeu o lancamento na linha de fundo",
+                "Jogador 3: Tenta passar para dentro da área",
+                "Jogador 1: Cheguei correndo no meio da área e recebe a bola",
+                "Jogador 1: Chuta para o gol e tenta a finalização",
+                "Goleiro 1: Agarra a bola e mata o lançe"
+            };
             
-            Singleton jogador2 = Singleton.GetInstancia;
-            jogador2.Mensagem("Jogador 2: Recebeu a bola, fiz o lancamento");
+            Logger logger1 = Logger.GetLogger;
+            logger1.Logar("Sincrono");
+            logger1.Logar(eventosPartida[0]);
+            
+            Logger logger2 = Logger.GetLogger;
+            logger2.Logar(eventosPartida[1]);
 
-            Singleton jogador3 = Singleton.GetInstancia;
-            jogador3.Mensagem("Jogador 3: Recebeu o lancamento na linha de fundo");
+            Logger logger3 = Logger.GetLogger;
+            logger3.Logar(eventosPartida[2]);
+            Console.ReadKey();
+
+            logger1.Logar("Assincrono");
+            Logger logger4 = Logger.GetLogger;
+            Parallel.ForEach(eventosPartida, eventoPartida =>
+            {
+                logger4.Logar(eventoPartida);
+            });
 
             Console.ReadKey();
         }
