2ec55bd9fd26bb1b63dba3a437bbe526bc9c4390
Pedro Alves <pedromsilvaalves@gmail.com>
2020-01-27 21:50:23 -0300

    Implementando AbstractFactory

---
+++ AbstractFactory/CarroPopularFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    public class CarroPopularFactory : CarroFactory
+    {
+        public override Roda montarRoda()
+        {
+            return new RodaSimples();
+        }
+
+        public override Som montarSom()
+        {
+            return new CDPlayer();
+        }
+    }
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var carro1 = ExecutaAbstractFactory.montarCarro("luxo");
+            var carro2 = ExecutaAbstractFactory.montarCarro("popular");
+
+            Console.ReadKey();
+        }
+    }
+}
